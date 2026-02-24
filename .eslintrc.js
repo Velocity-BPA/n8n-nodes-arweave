4eccd1b1cfaa1bcb3d6e18c70c722cd07ba5a5c1
John Drefahl <john@velobpa.com>
2026-02-24 04:30:39 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,15 @@
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
+ */
+
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'n8n-nodes-base'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  env: {
-    node: true,
-    es2020: true,
-    jest: true,
-  },
-  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'no-console': 'off',
-    'prefer-const': 'error',
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
   },
-  overrides: [
-    {
-      files: ['**/*.node.ts'],
-      rules: {
-        'n8n-nodes-base/node-class-description-credentials-name-unsuffixed': 'off',
-        'n8n-nodes-base/node-class-description-display-name-unsuffixed-trigger-node': 'off',
-      },
-    },
-  ],
 };
